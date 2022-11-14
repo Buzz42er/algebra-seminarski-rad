79d12d9debb7d92f71be779846d0e2a358862a59
Buzz <101808165+Buzz42er@users.noreply.github.com>
2022-11-14 18:57:48 +0100

    Updating App

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,53 @@ import ChatRoom from './components/ChatRoom';
 import RecivedMessage from './components/RecivedMessage';
 import SentMessage from './components/SentMessage';
 import NavListItem from './components/NavListItem';
+import { useEffect } from 'react';
+
 function App() {
+//   const drone = new Scaledrone('KRxTFNfTQOZwor7e');
+//   const room = drone.subscribe('chat1');
+
+//   //Listening to message
+// room.on('open', error => {
+//   if (error) {
+//     return console.error(error);
+//   }
+//   // Connected to room
+// });
+
+// room.on('message', message => {
+//   // Received a message sent to the room
+// });
+
+// //sending the message
+// const message = {
+//   hello: 'world',
+//   score: 10
+// };
+
+// drone.publish({
+//   room: 'room_name',
+//   message: message
+// });
+
+
+
+useEffect(() => {
+  const drone = new Scaledrone('KRxTFNfTQOZwor7e');
+drone.on('error', error => {
+  console.error('Error with connection:', error);
+});
+drone.on('close', event => {
+  console.log('Connection closed:', event);
+});
+
+const room = drone.subscribe('my-room');
+room.on('message', message => console.log('Received data:', message.data));
+})
+
+const sendMessage = () => {
+  
+}
 
   return (
     <div className="App">
@@ -30,12 +76,12 @@ function App() {
           {/* current chat */}
           <div className='messages-div'>
             {/* message area */}
-            <RecivedMessage/>
+            <RecivedMessage />
             <SentMessage/>
           </div>
           <div className='input-div'>
             {/* input area */}
-            <input type="text" placeholder='Start typing...'/>
+            <input type="text" placeholder='Start typing...' onClick={sendMessage}/>
             <button className='send'>SEND</button>
           </div>
         </section>
